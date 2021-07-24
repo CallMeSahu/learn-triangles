bf3adbc03a137212bb9ae11b21b02e6810ddde49
Siddhartha <siddharthasahu.work@gmail.com>
2021-07-25 00:46:10 +0530

    fix: angles.js

--- angles.js
+++ angles.js
@@ -6,23 +6,6 @@ var result = document.querySelector("#triangle-output");
 var triangleTypeOutput = document.querySelector("#triangle-type");
 var x,y,z = 0;
 
-function getTriangle(){
-    if((firstAngleInput.value == "")||(secondAngleInput.value == "")||(thirdAngleInput.value == "")){
-        alert("Enter all angles!");
-        return
-    }    
-    x = parseInt(firstAngleInput.value, 10);
-    y = parseInt(secondAngleInput.value, 10);
-    z = parseInt(thirdAngleInput.value, 10);    
-    var angle = x+y+z;
-    if(angle === 180){
-        return "Yes, it is a Triangle!"
-    }
-    else{
-        return "No, it is not a Triangle."
-    }
-}
-
 function getTriangleType(){
     if((x==90)||(y==90)||(z==90)){
         return "Right";
@@ -34,8 +17,24 @@ function getTriangleType(){
         return "Obtuse";
 }
 
-findBtn.addEventListener("click", () =>{   
-    console.log("click"); 
-    result.innerText = getTriangle();
-    triangleTypeOutput.innerText = getTriangleType();
+function getTriangle(){    
+    x = parseInt(firstAngleInput.value, 10);
+    y = parseInt(secondAngleInput.value, 10);
+    z = parseInt(thirdAngleInput.value, 10);    
+    var angle = x+y+z;
+    if(angle === 180){
+        triangleTypeOutput.innerText = getTriangleType();
+        return "Yes, it is a Triangle!"
+    }
+    else{
+        return "No, it is not a Triangle."
+    }
+}
+
+findBtn.addEventListener("click", () =>{ 
+    if((firstAngleInput.value == "")||(secondAngleInput.value == "")||(thirdAngleInput.value == "")){
+        alert("Enter all angles!");
+        return
+    }        
+    result.innerText = getTriangle();    
 });
