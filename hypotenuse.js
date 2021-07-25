267c330ffb9a77bb82714abaf7e22648a8753cd8
Siddhartha <siddharthasahu.work@gmail.com>
2021-07-25 21:43:26 +0530

    feat: hypotenuse

---
+++ hypotenuse.js
@@ -0,0 +1,21 @@
+var sideOne = document.querySelector("#side-one");
+var sideTwo = document.querySelector("#side-two");
+var sideThree = document.querySelector("#side-three");
+var findBtn = document.querySelector("#find-button");
+
+function getThirdSide(){
+    var a = parseInt(sideOne.value, 10);
+    var b = parseInt(sideTwo.value, 10);
+    var result = Math.sqrt(a*a + b*b);
+    return result;
+}
+
+findBtn.addEventListener("click", () => {
+    if((sideOne.value == "") || (sideTwo.value == "")){
+        alert("Fill all sides!");
+        return;
+    }
+    else{
+        sideThree.innerText = getThirdSide();
+    }
+});
